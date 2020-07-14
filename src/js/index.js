5d9f02554560644cc89a5f077e582a4370298197
Renato Junior <momentum323@gmail.com>
2020-07-14 12:19:44 -0300

    final project

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,47 @@
 window.addEventListener('load', start)
 
-let globalName =['um', 'dois', 'tres', 'quatro']
+let globalName = ['um', 'dois', 'tres', 'quatro']
 let inputName = null
+let isEdit = false
+let currentIndex
 
-
-function start(){
+function start () {
   inputName = document.querySelector('input')
   preventFormSubmit()
   activeInputName()
   render()
-
-  
 }
 
-
-
-function preventFormSubmit(){
+function preventFormSubmit () {
   const form = document.querySelector('form')
-  function handleFormSubmit(event){
+  function handleFormSubmit (event) {
     event.preventDefault()
   }
-  form.addEventListener('submit',handleFormSubmit)
+  form.addEventListener('submit', handleFormSubmit)
 }
 
-function activeInputName(){
-  function insertName(name){
+function activeInputName () {
+  function insertName (name) {
     globalName.push(name)
-    render()
   }
-  function handleTyping(event){
-    if(event.key === 'Enter'){
-      insertName(event.target.value)
+
+  function updateName(newName) {
+    globalName[currentIndex] = newName
+
+  }
+
+
+  function handleTyping (event) {
+    if (event.key === 'Enter') {
+      if (isEdit) {
+        updateName(event.target.value)
+      } else {
+        insertName(event.target.value)
+      }
+      
+      render()
+      isEdit = false
+      clearInput()
     }
   }
 
@@ -38,17 +49,16 @@ function activeInputName(){
   inputName.focus()
 }
 
-function render(){
+function render () {
   const ul = document.querySelector('ul')
   ul.innerHTML = ''
 
-  
-  for (let i = 0; i<globalName.length; i++) {
+  for (let i = 0; i < globalName.length; i++) {
     let currentName = globalName[i]
     const li = document.createElement('li')
 
-    function deleteButtonName(index){
-      function deleteName(){
+    function deleteButtonName (index) {
+      function deleteName () {
         globalName.splice(index, 1)
         render()
       }
@@ -56,27 +66,39 @@ function render(){
       const btx = document.createElement('button')
       btx.textContent = 'x'
       btx.classList.add('buttonDelete')
-      
+
       btx.addEventListener('click', deleteName)
 
       return btx
-
     }
 
-    const span = document.createElement('span')
-    span.textContent = currentName
+    function createSpan (name, index) {
+      function editItem () {
+        inputName.value = name
+        inputName.focus()
+        isEdit = true
+        currentIndex = index
+      }
+
+      const span = document.createElement('span')
+      span.textContent = name
+      span.classList.add('clickble')
+
+      span.addEventListener('click', editItem)
+
+      return span
+    }
 
     const btx = deleteButtonName(i)
-
+    const span = createSpan(currentName, i)
     li.appendChild(btx)
     li.appendChild(span)
     ul.appendChild(li)
     clearInput()
   }
-  
 }
 
-function clearInput(){
+function clearInput () {
   inputName.value = ''
   inputName.focus()
 }
